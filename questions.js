6d21c02c77a9c8c9ae6e21cae69755b0b533d9d2
Khushboo <khushboodhiman716@gmail.com>
2021-08-24 16:02:02 +0530

    Display next question on next button click

---
+++ questions.js
@@ -0,0 +1,32 @@
+const questionList = [
+    {
+        number: 1,
+        question: "How many times was Ross divorced?",
+        option: [
+            "1",
+            "2",
+            "3",
+            "4"],
+        correct: "3"
+    },
+    {
+        number: 2,
+        question: "Where was Rachel an assistant buyer?",
+        option: [
+            "Macy’s",
+            "Saks Fifth Avenue",
+            "Bloomingdale's",
+            "Neiman Marcus"],
+        correct: "Bloomingdale's"
+    },
+    {
+        number: 3,
+        question: "Finish the line: I mean, isn't that just ______?",
+        "option": [
+            "kick-you-in-the-crotch, spit-on-your-neck fantastic?",
+            "kick-you-in-the-gut, spit-on-your-face fantastic?",
+            "kick-you-in-the-shin, yank-out-your-hair fantastic?",
+            "kick-you-in-the-knee, slap-you-on-your-face fantastic?"],
+        correct: "kick-you-in-the-crotch, spit-on-your-neck fantastic?"
+    },
+];
--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-let hello = "Hello";
+let questionBlock = document.querySelector('.question');
+let optionsBlock = document.querySelectorAll('.answer');
+let questionNumber = document.querySelector('.number');
+let n = 0;
+
+
+displayQuestion(n);
+
+
+function displayQuestion(n) {
+    console.log(n);
+    questionNumber.innerHTML = questionList[n]['number'];
+    questionBlock.innerHTML = questionList[n]['question'];
+    optionsBlock[0].innerHTML = questionList[n]['option'][0];
+    optionsBlock[1].innerHTML = questionList[n]['option'][1];
+    optionsBlock[2].innerHTML = questionList[n]['option'][2];
+    optionsBlock[3].innerHTML = questionList[n]['option'][3];
+};
+
+
+document.querySelector('.next-btn').onclick = function askNextQuestion() {
+    n++;
+    displayQuestion(n);
+}
+
