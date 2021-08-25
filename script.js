43c5f8f1be139a63ddd707145a78cbfcb224423b
Khushboo <khushboodhiman716@gmail.com>
2021-08-25 12:34:47 +0530

    All done

--- script.js
+++ script.js
@@ -1,57 +1,63 @@
 let questionBlock = document.querySelector('.question');
 let optionsBlock = document.querySelectorAll('.answer');
-let questionNumber = document.querySelector('.number');
+let questionNumberBlock = document.querySelector('.number');
+let scoreBlock = document.querySelector('.score-badge');
+let maxScoreBlock = document.querySelector('.max');
+let maxQuestion = questionList.length;
 let n = 0;
 let userEntered = '';
 let correct = '';
-
+let score = 0;
+scoreBlock.innerHTML = score;
+maxScoreBlock.innerHTML = maxQuestion;
 
 displayQuestion(n);
 
-
 function displayQuestion(n) {
-    questionNumber.innerHTML = questionList[n]['number'];
+    let questionNumber = questionList[n]['number'];
+    checkIfLast(questionNumber);
+    questionNumberBlock.innerHTML = questionNumber;
     questionBlock.innerHTML = questionList[n]['question'];
     optionsBlock[0].innerHTML = questionList[n]['option'][0];
     optionsBlock[1].innerHTML = questionList[n]['option'][1];
     optionsBlock[2].innerHTML = questionList[n]['option'][2];
     optionsBlock[3].innerHTML = questionList[n]['option'][3];
     correct = questionList[n]['correct'];
-    console.log('correct', correct)
 };
 
 
 for (let i = 0; i < optionsBlock.length; i++) {
     optionsBlock[i].onclick = function checkAnswer() {
         document.querySelector('.next-btn').disabled = false;
-        console.log(correct);
         userEntered = this.innerHTML;
-        if (userEntered == correct)
-            showCorrect(this, correct);
+        if (userEntered == correct) {
+            showScore();
+            showCorrect(this);
+        }
         else
-            showWrong(this, userEntered, correct);
+            showWrong(this);
     }
 }
 
 function findCorrect() {
     for (let i = 0; i < optionsBlock.length; i++) {
-        console.log(correct);
         if (optionsBlock[i].innerHTML == correct) {
-            console.log("found the riht one");
             return optionsBlock[i];
         }
     }
 }
 
-function showCorrect(option, correct) {
-    console.log(option, correct);
+function showCorrect(option) {
     option.classList.add('correct');
 }
 
-function showWrong(option, userEntered, correct) {
-    console.log(option, userEntered, correct);
+function showWrong(option) {
     option.classList.add('wrong');
     findCorrect().classList.add('correct');
+}
+
+function showScore() {
+    scoreBlock.innerHTML = ++score;
 }
 
 function resetEverything() {
@@ -64,8 +70,34 @@ function resetEverything() {
     }
 }
 
+
+function checkIfLast(questionNumber) {
+    console.log(questionNumber, maxQuestion);
+    if (questionNumber == maxQuestion) {
+        document.querySelector('.next-btn').value = "Finish Quiz";
+        document.querySelector('.next-btn').classList.add('finish-btn');
+        document.querySelector('.finish-btn').onclick = () => {
+            console.log("oye", score, maxQuestion, scoreBlock, maxScoreBlock);
+            let resultScoreBlock = document.querySelector('.result-score-badge');
+            let resultMaxBlock = document.querySelector('.result-max');
+            resultScoreBlock.innerHTML = score;
+            resultMaxBlock.innerHTML = maxQuestion;
+            console.log(score, maxQuestion);
+            document.querySelector('.quiz-wrapper').style.display = "none";
+            document.querySelector('.results').style.display = "flex";
+            scoreBlock = document.querySelector('.score-badge');
+            maxScoreBlock = document.querySelector('.max');
+        }
+    }
+}
+
+
 document.querySelector('.next-btn').onclick = function askNextQuestion() {
     n++;
     displayQuestion(n);
     resetEverything();
+}
+
+document.querySelector('.retake').onclick = function retake() {
+    location.reload();
 }
